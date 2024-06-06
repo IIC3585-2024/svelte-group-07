0d6fa73f1d28fd010515539c71ee0eab44ecbbe5
donjavi-538 <javier.v.armijo@uc.cl>
2024-06-05 23:35:41 -0400

    feat: BookCard component

---
+++ src/components/BookCard.svelte
@@ -0,0 +1,42 @@
+<script setup>
+
+export let title;
+export let authors = [];
+export let coverI;
+
+$: authorsString = authors.join(', ').trimEnd();
+$: coverURL = `http://covers.openlibrary.org/b/id/${coverI}-M.jpg`;
+    
+</script>
+    
+<div class="card">
+    <img src={coverURL} alt="book cover" class="cover"/>
+    <h2>{ title }</h2>
+    <p>{ authorsString }</p>
+</div>
+
+<style>
+.card {
+    background-color: #2f2f2f;
+    border-radius: 5px;
+    color: #fff;
+    padding: 10px;
+    margin: 10px;
+    width: 18rem;
+    text-align: center;
+    text-wrap: wrap;
+    height: auto;
+}
+
+.card:hover {
+    cursor: pointer;
+}
+
+.cover {
+    /* max height of 260px */
+    width: 100%;
+    height: 16rem;
+    object-fit: cover;
+}
+
+</style>
