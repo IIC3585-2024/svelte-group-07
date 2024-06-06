a3c48321aa0c3a17016ea0340c97c9f6ee718d29
Pedro Palma <pedro.palma@uc.cl>
2024-06-06 10:32:16 -0400

    style: add margin top to board

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,5 +2,12 @@
     import CategoryBoard from "../components/CategoryBoard.svelte";
 </script>
 
-
+<div class="container">
     <CategoryBoard />
+</div>
+
+<style>
+    .container {
+        margin-top: 100px;
+    }
+</style>
