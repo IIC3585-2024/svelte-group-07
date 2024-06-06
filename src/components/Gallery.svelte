010ae22c03badd0036820eba8c6af0a2de8882f9
Jimnora <djimnez@uc.cl>
2024-06-06 03:48:48 -0400

    feat: Add functional cards to gallery

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -2,24 +2,55 @@
     // get the category id from the URL
     import { categories } from "../data/categories.js";
     import { onMount } from "svelte";
+    import BookCard from "./BookCard.svelte";
+    import { db } from "../db.js";
 
     let category = {};
+    let books = {};
+
+    async function getBooksByCategory(categoryId) {
+        if (!categoryId) return;
+        else if (categoryId === "all") {
+            const booksArray = await db.books.toArray();
+            books = booksArray;
+            return;
+        }else{
+            const booksArray = await db.books.where("category").equals(categoryId).toArray();
+            books = booksArray;
+        }
+    }
 
     onMount(() => {
         const id = window.location.pathname.split("/").pop();
         category = categories.find((category) => category.id === id);
     });
-
-    console.log(category);
 </script>
 
-<div>
+<div class="container">
     <h1> {category.name} </h1>
     <div class="gallery">
+        {#await getBooksByCategory(category.id)}
+            <p>Loading...</p>            
+        {:then} 
+            {#each books as book, i}
+                <a href="/book/{book.id}">
+                    <BookCard book={book} />
+                </a>
+            {/each}
+        {/await}
     </div>
 </div>
 
 <style>
+
+.container {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    min-width: 70vw;
+    height: 90vh;
+    width: 100%;
+}
 
 .gallery {
     display: flex;
